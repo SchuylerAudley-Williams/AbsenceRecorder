0392a723d196cce617d80044cb290eb1aae2a06f
Schuyler Audley-Williams <audley-williams.s@etoncollege.org.uk>
2022-02-02 14:50:17 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Audley-Williams, Schuyler (SPH) on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
