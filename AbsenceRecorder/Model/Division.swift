ef18e722013322b87fd4efab21a162dcd439df56
Schuyler Audley-Williams <audley-williams.s@etoncollege.org.uk>
2022-02-02 15:11:06 +0000

    Adding groups and basic model for divisions and students

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,18 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Audley-Williams, Schuyler (SPH) on 02/02/2022.
+//
+
+import Foundation
+
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code:String) {
+        self.code = code
+    }
+}
---
+++ AbsenceRecorder/Model/Student.swift
@@ -0,0 +1,20 @@
+//
+//  Student.swift
+//  AbsenceRecorder
+//
+//  Created by Audley-Williams, Schuyler (SPH) on 02/02/2022.
+//
+
+import Foundation
+
+class Student {
+    let forename: String
+    let surname: String
+    let birthday: String
+    
+    init(forename: String, surname: String, birthday: String) {
+        self.forename = forename
+        self.surname = surname
+        self.birthday = birthday
+    }
+}
